6d3687fcf0b7b718ae97172838387808be834d21
Aïsha <97292962+IbAisha@users.noreply.github.com>
2023-06-09 15:47:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecole La Plateforme, le Campus Méditeranéen du Numérique</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<nav>     
+    <img src="img/logo_lp.png" id="logo" alt="Logo Laplateforme" width="150px">     
+        <ul>
+            <li><a href="#">Qui sommes-nous ?<a></li>
+            <li><a href="#">Cursus</a></li>
+            <li><a href="#">Spécialisation</a></li>
+            <li><a href="#">Entreprise</a></li>
+            <li><a href="#">Kids</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Français</a></li>
+        </ul>
+    </nav>
+
+<main>
+    <section id="texte">
+        <p>
+        <strong>La Plateforme</strong> le campus méditerranéen du numérique ! </br></br>
+        Des parcours frais de scolarité à zéro euros, ouverts à tous les talents, sans conditions de ressources et de diplômes.</br></br>
+        Un lieu de rencontre entre entreprises et apprenants pour le bénéfice de tous !</br></br>
+        </p>
+        <p>Le digital c'est maintenant !</p>
+
+            <button><a href ="inscription.php">Les inscriptions sont ouvertes !*</a></button>
+    </section>
+
+    <section id="images">
+        <img src="img/perso_lp.jpg" alt="personne profile">
+    </section>
+
+    
+</main>
+   
+
+</body>
+
+</html>
