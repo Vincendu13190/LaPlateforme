d60f3f678b88720548d3936aa9e3a55e5e5b6ca2
Aïsha <97292962+IbAisha@users.noreply.github.com>
2023-06-09 10:29:03 +0200

    Add files via upload
    Partie page d'accueil mobile/tablet -v

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecole La Plateforme, le Campus Méditeranéen du Numérique</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<main>
+    <div id="text" width="100%">
+        <p>
+        <strong>La Plateforme</strong> le campus méditerranéen du numérique ! </br></br>
+        Des parcours frais de scolarité à zéro euros, ouverts à tous les talents, sans conditions de ressources et de diplômes.</br></br>
+        Un lieu de rencontre entre entreprises et apprenants pour le bénéfice de tous !</br></br>
+        </p>
+        <p>Le digital c'est maintenant !</p>
+            <button>Les inscriptions sont ouvertes !*</button>
+    </div>
+
+   
+
+    <div id="images">
+        <img src="img/perso_lp.jpg" alt="personne profile" width="100%">
+    </div>
+
+    
+</main>
+   
+
+</body>
+
+</html>
